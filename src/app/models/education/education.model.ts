a10eca41c5b4a8942a7f0dfc885aacc61c82ea0c
giovanni760 <giovanni162005@gmail.com>
2025-03-12 15:34:09 -0600

    update services

---
+++ src/app/models/education/education.model.ts
@@ -0,0 +1,6 @@
+export class Education{
+	id?: string;
+	highSchool?: 'item1, item2, item n';
+	university?: 'item1, item2, item n';
+
+}
---
+++ src/app/models/skills/skills.model.ts
@@ -0,0 +1,5 @@
+export class Skills{
+  id?: string;
+  dateBase?:'item1, item2, item n'; 
+  programming?: 'item1, item2, item n';
+}
